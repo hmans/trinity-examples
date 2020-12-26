0fd79713f3a58dff6fba50c4b3a9de8081df4833
Hendrik Mans <hendrik@mans.de>
2020-12-26 21:15:15 +0000

    GLTF demo

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "wouter"
 import { examples } from "./examples"
+import styles from "./Navigation.module.css"
 
 export const Navigation = () => (
-  <div>
+  <div className={styles.Navigation}>
     {examples.map(([path, name, _]) => (
       <Link key={path} to={path}>
         {name}
--- src/examples.ts
+++ src/examples.ts
@@ -1,3 +1,7 @@
+import { GLTFExample } from "./examples/GLTFExample"
 import { SimpleExample } from "./examples/SimpleExample"
 
-export const examples: [string, string, Function][] = [["/simple", "Simple Example", SimpleExample]]
+export const examples: [string, string, Function][] = [
+  ["/simple", "Simple Example", SimpleExample],
+  ["/gltf", "GLTF Loading", GLTFExample]
+]
