a64deb65bff23712908913055a37021ad595738d
Hendrik Mans <hendrik@mans.de>
2020-12-27 19:33:13 +0000

    Styles

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -5,8 +5,16 @@ import styles from "./Navigation.module.css"
 
 export const Navigation = () => (
   <div className={styles.Navigation}>
+    <p>
+      A collection of examples for (and experiments with) the{" "}
+      <a href="https://github.com/hmans/trinity" target="_blank" rel="noreferrer">
+        <strong>Trinity</strong>
+      </a>{" "}
+      framework.
+    </p>
+
     {examples.map((category, i) => (
-      <div key={i}>
+      <div key={i} className="category">
         <h3>{category.name}</h3>
         {category.examples.map(([path, name, _]) => (
           <Link key={path} to={path}>
