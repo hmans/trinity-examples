40860167b24b3af73929004640e0492452956220
Hendrik Mans <hendrik@mans.de>
2020-12-27 17:04:15 +0000

    Further tweaks

--- src/SourceCode.tsx
+++ src/SourceCode.tsx
@@ -6,14 +6,22 @@ import styles from "./SourceCode.module.css"
 
 SyntaxHighlighter.registerLanguage("typescript", ts)
 
-const fullSourceUrl = (path: string) =>
-  `https://github.com/hmans/trinity-examples/tree/master/src/${path}`
+const GithubLink: React.FC<{ path: string }> = ({ path, children }) => (
+  <a
+    href={`https://github.com/hmans/trinity-examples/tree/master/src/${path}`}
+    target="_blank"
+    rel="noreferrer"
+  >
+    {children}
+  </a>
+)
 
-const rawSourceUrl = (path: string) =>
-  `https://raw.githubusercontent.com/hmans/trinity-examples/master/src/${path}`
-
-const SourceCodeLink: React.FC<{ path: string }> = ({ path, children }) => (
-  <a href={fullSourceUrl(path)} target="_blank" rel="noreferrer">
+const CodesandboxLink: React.FC<{ path: string }> = ({ path, children }) => (
+  <a
+    href={`https://codesandbox.io/s/trinity-examples-7v8np?file=/src/${path}`}
+    target="_blank"
+    rel="noreferrer"
+  >
     {children}
   </a>
 )
@@ -22,17 +30,20 @@ export const SourceCode: React.FC<{ path: string }> = ({ path }) => {
   const [source, setSource] = useState<string>()
 
   useEffect(() => {
-    fetch(rawSourceUrl(path)).then((response) => {
-      response.text().then((text) => {
-        setSource(text)
-      })
-    })
+    fetch(`https://raw.githubusercontent.com/hmans/trinity-examples/master/src/${path}`).then(
+      (response) => {
+        response.text().then((text) => {
+          setSource(text)
+        })
+      }
+    )
   }, [path])
 
   return (
     <div className={styles.SourceCode}>
       <p>
-        <SourceCodeLink path={path}>Open on GitHub</SourceCodeLink>
+        <GithubLink path={path}>GitHub</GithubLink> &middot;{" "}
+        <CodesandboxLink path={path}>Codesandbox</CodesandboxLink>
       </p>
       {source && (
         <SyntaxHighlighter language="typescript" style={style}>
