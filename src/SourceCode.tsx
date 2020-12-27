b6c7d6095427e1e3aa2a6efa41b8e8854f32818c
Hendrik Mans <hendrik@mans.de>
2020-12-27 12:59:40 +0000

    Load Source Code from Github

--- src/SourceCode.tsx
+++ src/SourceCode.tsx
@@ -1,20 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styles from "./SourceCode.module.css"
 
+const fullSourceUrl = (path: string) =>
+  `https://github.com/hmans/trinity-examples/tree/master/src/${path}`
+
+const rawSourceUrl = (path: string) =>
+  `https://raw.githubusercontent.com/hmans/trinity-examples/master/src/${path}`
+
 const SourceCodeLink: React.FC<{ path: string }> = ({ path, children }) => (
-  <a
-    href={`https://github.com/hmans/trinity-examples/tree/master/src/${path}`}
-    target="_blank"
-    rel="noreferrer"
-  >
+  <a href={fullSourceUrl(path)} target="_blank" rel="noreferrer">
     {children}
   </a>
 )
 
 export const SourceCode: React.FC<{ path: string }> = ({ path }) => {
+  const [source, setSource] = useState<string>()
+
+  useEffect(() => {
+    fetch(rawSourceUrl(path)).then((response) => {
+      response.text().then((text) => setSource(text))
+    })
+  })
+
   return (
     <div className={styles.SourceCode}>
       <SourceCodeLink path={path}>Source Code</SourceCodeLink>
+      {source && <pre>{source}</pre>}
     </div>
   )
 }
